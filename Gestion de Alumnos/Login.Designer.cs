5bb3e1638ae8a2dab0ad39f8da83332a91deee9b
matias agustin duarte <duarte-matias@live.com.ar>
2018-09-28 11:44:05 -0300

    Solucionado problema con VB Powershell packs

--- Gestion de Alumnos/Login.Designer.cs
+++ Gestion de Alumnos/Login.Designer.cs
@@ -32,9 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pnlLogoContainer = new System.Windows.Forms.Panel();
             this.pbLogo = new System.Windows.Forms.PictureBox();
-            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.lsContraseña = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lsUsuario = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPass = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,6 +40,8 @@
             this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.btnMinimizar = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtLineasUsuario = new System.Windows.Forms.TextBox();
+            this.txtLineasPass = new System.Windows.Forms.TextBox();
             this.pnlLogoContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
@@ -69,41 +68,6 @@
             this.pbLogo.TabIndex = 0;
             this.pbLogo.TabStop = false;
             // 
-            // shapeContainer1
-            // 
-            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
-            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
-            this.shapeContainer1.Name = "shapeContainer1";
-            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
-            this.lsContraseña,
-            this.lsUsuario});
-            this.shapeContainer1.Size = new System.Drawing.Size(780, 330);
-            this.shapeContainer1.TabIndex = 1;
-            this.shapeContainer1.TabStop = false;
-            // 
-            // lsContraseña
-            // 
-            this.lsContraseña.BorderColor = System.Drawing.Color.DimGray;
-            this.lsContraseña.Cursor = System.Windows.Forms.Cursors.Default;
-            this.lsContraseña.Enabled = false;
-            this.lsContraseña.Name = "lsContraseña";
-            this.lsContraseña.SelectionColor = System.Drawing.Color.DimGray;
-            this.lsContraseña.X1 = 340;
-            this.lsContraseña.X2 = 727;
-            this.lsContraseña.Y1 = 217;
-            this.lsContraseña.Y2 = 217;
-            // 
-            // lsUsuario
-            // 
-            this.lsUsuario.BorderColor = System.Drawing.Color.DimGray;
-            this.lsUsuario.Enabled = false;
-            this.lsUsuario.Name = "lsUsuario";
-            this.lsUsuario.SelectionColor = System.Drawing.Color.DimGray;
-            this.lsUsuario.X1 = 340;
-            this.lsUsuario.X2 = 723;
-            this.lsUsuario.Y1 = 137;
-            this.lsUsuario.Y2 = 137;
-            // 
             // txtUser
             // 
             this.txtUser.BackColor = System.Drawing.Color.Black;
@@ -112,7 +76,7 @@
             this.txtUser.ForeColor = System.Drawing.Color.DimGray;
             this.txtUser.Location = new System.Drawing.Point(340, 113);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(380, 24);
+            this.txtUser.Size = new System.Drawing.Size(284, 24);
             this.txtUser.TabIndex = 1;
             this.txtUser.Text = "USUARIO";
             this.txtUser.Enter += new System.EventHandler(this.txtUser_Enter);
@@ -124,7 +88,7 @@
             this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtPass.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPass.ForeColor = System.Drawing.Color.DimGray;
-            this.txtPass.Location = new System.Drawing.Point(340, 193);
+            this.txtPass.Location = new System.Drawing.Point(340, 194);
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(380, 24);
             this.txtPass.TabIndex = 2;
@@ -200,21 +164,44 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // txtLineasUsuario
+            // 
+            this.txtLineasUsuario.BackColor = System.Drawing.Color.Black;
+            this.txtLineasUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLineasUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.txtLineasUsuario.Location = new System.Drawing.Point(341, 125);
+            this.txtLineasUsuario.Name = "txtLineasUsuario";
+            this.txtLineasUsuario.Size = new System.Drawing.Size(379, 13);
+            this.txtLineasUsuario.TabIndex = 7;
+            this.txtLineasUsuario.Text = "______________________________________________________________________";
+            // 
+            // txtLineasPass
+            // 
+            this.txtLineasPass.BackColor = System.Drawing.Color.Black;
+            this.txtLineasPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLineasPass.ForeColor = System.Drawing.Color.DimGray;
+            this.txtLineasPass.Location = new System.Drawing.Point(341, 206);
+            this.txtLineasPass.Name = "txtLineasPass";
+            this.txtLineasPass.Size = new System.Drawing.Size(379, 13);
+            this.txtLineasPass.TabIndex = 7;
+            this.txtLineasPass.Text = "______________________________________________________________________";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtLineasPass);
+            this.Controls.Add(this.txtLineasUsuario);
             this.Controls.Add(this.btnMinimizar);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.lblRecuperarContraseña);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.txtUser);
             this.Controls.Add(this.pnlLogoContainer);
-            this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.Opacity = 0.9D;
@@ -233,10 +220,7 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlLogoContainer;
-        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
-        private Microsoft.VisualBasic.PowerPacks.LineShape lsUsuario;
         private System.Windows.Forms.TextBox txtUser;
-        private Microsoft.VisualBasic.PowerPacks.LineShape lsContraseña;
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnIngresar;
@@ -245,6 +229,8 @@
         private System.Windows.Forms.PictureBox btnMinimizar;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.TextBox txtLineasUsuario;
+        private System.Windows.Forms.TextBox txtLineasPass;
     }
 }
 
